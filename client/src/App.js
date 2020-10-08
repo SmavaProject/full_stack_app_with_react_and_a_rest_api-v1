c21dd1f075d945ac47342af5ff1473aea27c4c7b
SmavaProject <katyakravchenka@yahoo.com>
2020-10-08 09:58:29 +0300

    add client

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import './App.css';
+
+class App extends Component {
+    render() {
+        console.log("rendering... ");
+        return (
+            < BrowserRouter >
+            < div
+        className = "container" >
+            < /div>
+            < /BrowserRouter>
+    )
+        ;
+    }
+}
+export default App;
