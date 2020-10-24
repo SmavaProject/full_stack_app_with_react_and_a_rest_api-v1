2d11b1b02c213ad1685ee2c66e2d963160d70363
SmavaProject <katyakravchenka@yahoo.com>
2020-10-23 20:22:43 -0700

    create

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
 import { NavLink } from 'react-router-dom';
 import {Link} from "react-router-dom";
+import ReactMarkdown from "react-markdown";
 
 class CourseDetail extends Component {
     state = {
-        courseDetail: {},
-        user: {}
+        courseDetail: {
+            user: {},
+            materialsNeeded: {}
+        }
     }
 
     componentDidMount() {
@@ -15,8 +18,9 @@ class CourseDetail extends Component {
         context.data.detCourseDetails(id).then((response => {
             if (response){
                 this.setState({
-                    courseDetail: response.course,
-                    user: response.course.owner,
+                    courseDetail: response,
+                    user: response.owner,
+                    materialsNeeded: response.materialsNeeded
                 });
             }else{
                 this.props.history.push("/error");
@@ -26,80 +30,87 @@ class CourseDetail extends Component {
         });
     }
 
+
+    userIsAuthenticated (authenticatedUser, user) {
+        return authenticatedUser.emailAddress === user.emailAddress
+    }
+
     render() {
 
         const {
             courseDetail,
-            user,
         } = this.state;
 
+        const user = courseDetail.user;
+        const materialsNeeded = courseDetail.materialsNeeded;
 
+        console.log("materialsNeeded " + materialsNeeded)
         const {context} = this.props;
         const {authenticatedUser} = context;
+
+        console.log("authenticatedUser.emailAddress " + authenticatedUser.emailAddress)
+        console.log("user.emailAddress " + user.emailAddress)
 
         const courseID = this.props.match.params.id;
         debugger;
         console.log("user " + user)
         console.log("courseDetail " + courseDetail)
+
         return (
 
-
-            <div className="action--bar">
-                <div className="bounds">
-                    <div className="grid-100">
-
-
+            <div>
+                <div className="action--bar">
+                    <div className="bounds">
+                        <div className="grid-100">
 
 
-
-                        <div className="bounds course--detail">
-                            <div className="grid-66">
-                                <div className="course--header">
-
-                                    <h4 className="course--label"> Course </h4>
-                                    <h3 className="course--title"> {courseDetail.title} </h3>
-                                    <h3> Owner </h3>
-                                    <h3> By {user.firstName} {user.lastName} </h3>
-                                    <div className="course--description">
-                                        <p> {courseDetail.description} </p>
-                                    </div>
-
-                                    <div className="grid-25 grid-right">
-                                        <div className="course--stats">
-                                            <ul className="course--stats--list">
-                                                <li className="course--stats--list--item">
-                                                    <h4>Estimated Time</h4>
-                                                    <h3> {courseDetail.estimatedTime} </h3>
-
-                                                </li>
-
-                                                <li className="course--stats--list--item">
-                                                    <h4>Materials Needed</h4>
-                                                    <ul>
-                                                        <li>1/2 x 3/4 inch parting strip</li>
-                                                        <li>1 x 2 common pine</li>
-                                                        <li>1 x 4 common pine</li>
-                                                        <li>1 x 10 common pine</li>
-                                                        <li>1/4 inch thick lauan plywood</li>
-                                                        <li>Finishing Nails</li>
-                                                        <li>Sandpaper</li>
-                                                        <li>Wood Glue</li>
-                                                        <li>Wood Filler</li>
-                                                        <li>Minwax Oil Based Polyurethane</li>
-                                                    </ul>
-                                                </li>
-                                            </ul>
-
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                            </div>
+                        <span>{this.userIsAuthenticated(authenticatedUser, user) ? (
+                                <React.Fragment>
+                                    <Link className="button" to={`/courses/${courseID}/update`}>Update Course</Link>
+                                    <Link className="button" to={`/courses/${courseID}/delete`}>Delete Course</Link>
+                                </React.Fragment>
+                            ) : <hr/>}
+                        </span>
+                            <Link className="button button-secondary" to="./">Return to List</Link>
 
                         </div>
                     </div>
+                </div>
+
+
+                <div className="bounds course--detail">
+                    <div className="grid-66">
+                        <div className="course--header">
+
+                            <h4 className="course--label"> Course </h4>
+                            <h3 className="course--title"> {courseDetail.title} </h3>
+                            <h3> Owner </h3>
+                            <h3> By {user.firstName} {user.lastName} </h3>
+                            <div className="course--description">
+                                <p> {courseDetail.description} </p>
+                            </div>
+
+                            <div className="grid-25 grid-right">
+                                <div className="course--stats">
+                                    <ul className="course--stats--list">
+                                        <li className="course--stats--list--item">
+                                            <h4>Estimated Time</h4>
+                                            <h3> {courseDetail.estimatedTime} </h3>
+
+                                        </li>
+
+                                        <li className="course--stats--list--item">
+                                            <h4>Materials Needed</h4>
+                                            <ReactMarkdown>{materialsNeeded}</ReactMarkdown>
+                                        </li>
+                                    </ul>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
 
