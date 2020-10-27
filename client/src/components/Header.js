f4036cef6e161e496ee38dd6fc89447853710526
SmavaProject <katyakravchenka@yahoo.com>
2020-10-26 22:07:56 -0700

    canged header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import React, {Component} from "react";
 
-export default class Header extends React.PureComponent {
+export default class Header extends Component {
     render() {
         const {context} = this.props;
         const user = context.authenticatedUser;
